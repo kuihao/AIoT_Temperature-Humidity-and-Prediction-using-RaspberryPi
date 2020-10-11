bf9a087fb4a9838162cc7cc58da8026e55df97db
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-10-11 20:31:52 +0800

    getHT with write sensor data in csv file correctly. :D

--- getHT.py
+++ getHT.py
@@ -1,24 +1,42 @@
-import sys
 import Adafruit_DHT
-import time
-import requests
+import csv
+import datetime
 import math
+import requests
+import sys
+import time
+'''
+function: write [datetime, temperature, humidity] in csv
+ipt: dhtwriter, temperature, humidity
+opt: none
+'''
+def WriteInCSV(dhtwriter, temperature, humidity):
+    dhtwriter.writerow([datetime.datetime.now().strftime('%y %m %d %H-%M-%S'),
+        '{:0.1f}'.format(temperature),
+        '{:0.1f}'.format(humidity)])
 
 # set Adafruit_DHT's varibles 
 sensor = Adafruit_DHT.DHT11
 pin = 4
 
-# get sencor data from DHT11
-try:
-    print('[INFO]press Ctrl-C to abort the process')
-    while True:
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-        # Try to grab a sensor reading.  Use the read_retry method which will retry up
-        # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-        if humidity is not None and temperature is not None:
-            print('[INFO]Temperature={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
-        else:
-            print('[ERROR]Failed to get reading. Try again!')
-        time.sleep(10)
-except KeyboardInterrupt:
-    print('[INFO]abort')
+# write in csv with append mode
+with open('dht11_record.csv', 'a') as dhtfile:
+    # create a csv writer object
+    dhtwriter = csv.writer(dhtfile, dialect='excel')
+    # get sencor data from DHT11
+    try:
+        print('[INFO]press Ctrl-C to abort the process')
+        while True:
+            # Get sensor data.  Use the read_retry method which will retry up
+            # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+            # Checking sensor dedect correctly
+            if humidity is not None and temperature is not None:
+                # function: write [datetime, temperature, humidity] in csv
+                WriteInCSV(dhtwriter, temperature, humidity)
+                print('[INFO]Temperature={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
+            else:
+                print('[ERROR]Failed to get reading. Try again!')
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print('[INFO]abort')
