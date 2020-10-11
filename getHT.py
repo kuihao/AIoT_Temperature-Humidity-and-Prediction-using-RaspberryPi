3ad6d2e51b9667c960c24d114fa14301c26db4f5
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-10-11 18:11:18 +0800

    getGT with debug chinese char.

--- getHT.py
+++ getHT.py
@@ -10,15 +10,15 @@ pin = 4
 
 # get sencor data from DHT11
 try:
-    print('按下 Ctrl-C 可停止程式')
+    print('press Ctrl-C to abort the process')
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, GPIO_PIN)
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
         if humidity is not None and temperature is not None:
-            print('溫度={0:0.1f}度C 濕度={1:0.1f}%'.format(temperature, humidity))
+            print('Temperature={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
         else:
-            print('讀取失敗，重新讀取。')
+            print('Failed to get reading. Try again!')
         time.sleep(10)
 except KeyboardInterrupt:
-    print('關閉程式')
+    print('abort')
