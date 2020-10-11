753af45d6999b7aac73d0ddcc0c60acb968300bc
Kuihao Chang <41053A041@gms.ndhu.edu.tw>
2020-10-11 18:18:05 +0800

    getHT work successfully and add literally detail in print out msg

--- getHT.py
+++ getHT.py
@@ -10,15 +10,15 @@ pin = 4
 
 # get sencor data from DHT11
 try:
-    print('press Ctrl-C to abort the process')
+    print('[INFO]press Ctrl-C to abort the process')
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
         if humidity is not None and temperature is not None:
-            print('Temperature={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
+            print('[INFO]Temperature={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
         else:
-            print('Failed to get reading. Try again!')
+            print('[ERROR]Failed to get reading. Try again!')
         time.sleep(10)
 except KeyboardInterrupt:
-    print('abort')
+    print('[INFO]abort')
